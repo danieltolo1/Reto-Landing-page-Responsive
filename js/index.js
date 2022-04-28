fcc5e31f685039549d44d26098eaacf8c4e96b72
danieltolo1 <danieltolo1@hotmail.com>
2022-04-27 22:32:05 -0500

    Lading page terminado

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const inicio =() =>{
+    alert("Inicio de Sesion exitoso")
+}
+
+const registro = () => {
+    alert("Esta siendo redirigido al registro en la plataforma")
+   };
+
+const documentos = () => {
+   alert("Esta siendo redirigido a la documentación")
+  };
